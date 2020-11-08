ab6a6369f2f0b262ff19580daa25ec9540ec4c6d
winkelco <57962651+winkelco@users.noreply.github.com>
2020-11-08 10:36:22 -0500

    Update

--- src/Exceptions/NotFoundExtensionConfigException.php
+++ src/Exceptions/NotFoundExtensionConfigException.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Viandwi24\LaravelExtension\Exceptions;
+namespace WinkelCo\LaravelExtension\Exceptions;
 
 use Exception;
 use Facade\IgnitionContracts\Solution;
 use Facade\IgnitionContracts\ProvidesSolution;
-use Viandwi24\LaravelExtension\Solutions\NotFoundExtensionConfigSolution;
+use WinkelCo\LaravelExtension\Solutions\NotFoundExtensionConfigSolution;
 
 class NotFoundExtensionConfigException extends Exception implements ProvidesSolution
 {
